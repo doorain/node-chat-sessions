8c1e7ef2d9f0017a42b1d8dfc319d336a7bdee67
Dorian Morgan <doorain@gmail.com>
2017-11-14 13:43:02 -0700

    fixed path to /build

--- server/index.js
+++ server/index.js
@@ -1,15 +1,45 @@
 const express = require('express');
+const sessions = require('express-session')
 const bodyParser = require('body-parser');
 const mc = require( `./controllers/messages_controller` );
+require('dotenv').config()
+
+//Middleware
+const createInitialSession = require( `./middleware/session` );
+const filter = require('./middleware/session');
 
 const app = express();
 
 app.use( bodyParser.json() );
-app.use( express.static( `${__dirname}/build` ) );
+
+console.log(__dirname)
+
+
+app.use( express.static( `${__dirname}/../build` ) );
+app.use(sessions({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized:true,
+  cookie:{
+    maxAge: 60000
+  }
+
+}))
+
+app.use(createInitialSession);
+app.use((req,res,next) => {
+  if(req.method === 'POST' || req.method === 'PUT'){
+    filter(req,res,next);
+  }
+  else{
+    next();
+  }
+})
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
 app.get( messagesBaseUrl, mc.read );
+app.get( messagesBaseUrl + '/history', mc.history)
 app.put( `${messagesBaseUrl}`, mc.update );
 app.delete( `${messagesBaseUrl}`, mc.delete );
 
